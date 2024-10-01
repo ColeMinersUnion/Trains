b0e930a4ef4048a442968f1612235cbe85788160
ColeMinersUnion <colehansen018@gmail.com>
2024-10-01 11:44:30 -0400

    Additional files for later use

---
+++ CTC_Office/Backend/Hook.py
@@ -0,0 +1,5 @@
+#import requests #! A library I'll use for get and post requests to hardware module
+#import asyncio #! Async function so that 
+#* I know I'm using react language but this is where I will interract with prashanth's
+#* Hardware. Hopefully I'll through this on an async function so it only runs when necessary
+#* and doesn't take a bunch of memory by running all the time on it's own thread
