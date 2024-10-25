b75fc4c8455788c190bc8a791cc00fbfb8503d72
Zpinball20 <zsf4@pitt.edu>
2024-10-25 16:20:51 -0400

    Updated Train Model with beacon handling

--- Train Model/TrainModelClass.py
+++ Train Model/TrainModelClass.py
@@ -12,12 +12,12 @@ class trainModel:
         self.acceleration = 0
         self.prevAcceleration = 0
         self.powerCommand = 0
-        self.Kp = 50
-        self.Ki = 10
+        self.powerCheck = False
         self.nextStation = "N/A"
-        self.authority = 0
-        self.doorStatus = False
+        self.leftDoorStatus = False
+        self.rightDoorStatus = False
         self.lightStatus = False
+        self.internalLightStatus = False
         self.engineFailure = False
         self.signalFailure = False
         self.brakeFailure = False
@@ -29,59 +29,60 @@ class trainModel:
         self.serviceBrake = False
         self.emergencyBrake = False
         self.period = float(os.getenv("PERIOD", 0.1))
-        self.prevSignal = 0
-        self.prevError = 0
         self.currentSpeed = 0
         self.prevSpeed = 0
-        self.goalSpeed = 0
-        self.error = 0
+        self.beaconData = "empty"
+        self.polarity = False
         self.temperature = 70 # in fahrenheit
-
-    # Setters and getters
-    def setGoalSpeed(self, input):
-        if self.signalFailure:
-            return
-        
-        input = input * 0.44704
-        
-        if(input > self.maxSpeed):
-            self.goalSpeed = self.maxSpeed
-        else:
-            self.goalSpeed = input
 
     def lightToggle(self):
         self.lightStatus = not self.lightStatus
 
-    def doorToggle(self):
-        self.doorStatus = not self.doorStatus
+    def getExternalLightStatus(self):
+        return self.lightStatus
 
-    def setNextStation(self, station):
-        if self.signalFailure:
-            return
-        
+    def internalLightToggle(self):
+        self.internalLightStatus = not self.internalLightStatus
+
+    def getInternalLightStatus(self):
+        return self.internalLightStatus
+
+    def leftDoorToggle(self):
+        self.leftDoorStatus = not self.leftDoorStatus
+
+    def getLeftDoorStatus(self):
+        return self.leftDoorStatus
+
+    def rightDoorToggle(self):
+        self.rightDoorStatus = not self.rightDoorStatus
+
+    def getRightDoorStatus(self):
+        return self.rightDoorStatus
+
+    def setNextStation(self, station):       
         self.nextStation = station
+
+    def setPowerCommand(self, input):
+        if(input > self.maxPower):
+            self.powerCommand = self.maxPower
+        else:
+            self.powerCommand = input
+
+        self.powerCheck = True
+
+    def getPowerCommand(self):
+        return self.powerCommand
 
     def servBrakeToggle(self):
         if self.brakeFailure:
             return
         self.serviceBrake = not self.serviceBrake
 
+    def getServBrakeStatus(self):
+        return self.serviceBrake
+
     def eBrakeToggle(self):
-        if self.brakeFailure:
-            return
         self.emergencyBrake = not self.emergencyBrake
-
-    def setCommandSpeed(self, input):
-        if self.signalFailure:
-            print("Signal Faiure ACTIVE")
-            return
-        self.commandSpeed = input
-
-    def setAuthority(self, input):
-        if self.signalFailure:
-            print("Signal Faiure ACTIVE")
-            return
-        self.authority = input
 
     def setTemp(self, input):
         self.temperature = input
@@ -91,14 +92,22 @@ class trainModel:
 
     def getCurrentSpeed(self):
         return self.currentSpeed
+    
+    def setBeaconData(self, input):
+        self.beaconData = input
+    
+    def getBeaconData(self):
+        return self.beaconData
+    
+    def changePolarity(self):
+        self.polarity = not self.polarity
+
+    def getPolarity(self):
+        return self.polarity
 
     # Imperial conversions
     def getCurrentSpeedImperial(self):
         return self.currentSpeed * 2.237
-
-    def getGoalSpeedImperial(self):
-        self.setGoalSpeed(self.goalSpeed)
-        return self.goalSpeed * 2.237
 
     def getAccelerationImperial(self):
         return self.acceleration * 3.28084
@@ -106,9 +115,6 @@ class trainModel:
     def getWeightImperial(self):
         self.totalMassCalc()
         return self.totalMass * 2.20462
-    
-    def getLength(self):
-        return self.length
     
     def getTemp(self):
         return self.temperature
@@ -122,27 +128,11 @@ class trainModel:
 
     def activateBrakeFailure(self):
         self.brakeFailure = not self.brakeFailure
-        # Print statement for debugging
 
     def totalMassCalc(self):
         self.totalMass = self.trainMass + (self.avgHumanMass * self.currentCapacity)
 
     # acceleration, power, and velocity calculation
-
-    def setGains(self, Ki, Kp):
-        self.Kp = Kp
-        self.Ki = Ki
-
-    # calculation for command power
-    def commandPowerCalc(self):
-        if(self.powerCommand < self.maxPower):
-            signal = self.prevSignal + ((self.period / 2) * (self.error + self.prevError))
-        else:
-            signal = self.prevSignal
-
-        self.powerCommand = (self.Kp * self.error) + (self.Ki * signal)
-
-        self.prevSignal = signal
 
     # calculation for acceleration
     def accelerationCalc(self):
@@ -154,74 +144,56 @@ class trainModel:
 
     # calculation for speed
     def speedCalculations(self):
-        tolerance = float(os.getenv("TOLERANCE", 0.01))
         
-        while True:
-            # Emergency Brake Handling
-            if self.emergencyBrake:
-                self.acceleration = -2.73
-                while self.currentSpeed > 0:
+        # Emergency Brake Handling
+        if self.emergencyBrake:
+            self.acceleration = -2.73
+            while self.currentSpeed > 0:
 
-                    if not self.emergencyBrake:
-                        self.acceleration = 0
-                        break
-
-                    if self.currentSpeed + self.acceleration < 0:
-                        self.currentSpeed = 0
-                    else:
-                        self.currentSpeed += self.acceleration
-                    time.sleep(1)
-                    self.goalSpeed = self.currentSpeed
-
-                self.acceleration = 0
-                break
-
-            # Service Brake Handling
-            elif self.serviceBrake:
-                self.acceleration = -1.2
-                while self.currentSpeed > 0:
-
-                    if not self.serviceBrake:
-                        self.acceleration = 0
-                        break
-
-                    if self.currentSpeed + self.acceleration < 0:
-                        self.currentSpeed = 0
-                    else:
-                        self.currentSpeed += self.acceleration
-                    time.sleep(1)
-                    self.goalSpeed = self.currentSpeed
-
-                self.acceleration = 0
-                break
-
-            # Engine Failure Handling
-            elif self.engineFailure:
-                if not self.serviceBrake and not self.emergencyBrake:
-                    self.acceleration = -0.01
-                    while self.currentSpeed > 0:
-
-                        if not self.engineFailure:
-                            self.acceleration = 0
-                            break
-
-                        self.currentSpeed += self.acceleration
-                        time.sleep(1)
-
-                    self.acceleration = 0
+                if self.currentSpeed + self.acceleration < 0:
                     self.currentSpeed = 0
+                else:
+                    self.currentSpeed += self.acceleration
+                time.sleep(1)
 
-            else:
-                if(self.goalSpeed > self.maxSpeed):
-                    self.goalSpeed = self.maxSpeed
-                
-                if abs(self.goalSpeed - self.currentSpeed) <= tolerance:
+            self.acceleration = 0
+
+        # Service Brake Handling
+        elif self.serviceBrake:
+            self.acceleration = -1.2
+            while self.currentSpeed > 0:
+                # Check for brake deactivation
+                if not self.serviceBrake:
+                    self.acceleration = 0
                     break
 
-                self.prevError = self.error
-                self.error = self.goalSpeed - self.currentSpeed
-                self.commandPowerCalc()
+                if self.currentSpeed + self.acceleration < 0:
+                    self.currentSpeed = 0
+                else:
+                    self.currentSpeed += self.acceleration
+                time.sleep(1)
 
+            self.acceleration = 0
+
+        # Engine Failure Handling
+        elif self.engineFailure:
+            if not self.serviceBrake and not self.emergencyBrake:
+                self.acceleration = -0.01
+                while self.currentSpeed > 0:
+                    # Check for engine failure
+                    if not self.engineFailure:
+                        self.acceleration = 0
+                        break
+
+                    self.currentSpeed += self.acceleration
+                    time.sleep(1)
+
+                self.acceleration = 0
+                self.currentSpeed = 0
+
+        else:
+            
+            if(self.powerCheck):
                 self.prevAcceleration = self.acceleration
                 self.accelerationCalc()
 
@@ -229,17 +201,6 @@ class trainModel:
 
                 self.currentSpeed = self.prevSpeed + (self.period / 2) * (self.acceleration + self.prevAcceleration)
 
-                #print(self.getAccelerationImperial())
+                print(self.getAccelerationImperial())
+                self.powerCheck = False
                 time.sleep(self.period)
-
-    # used to check if service brake needs to turn on
-    def authorityCalc(self):
-        distanceToZero = ((self.currentSpeed) ** 2) /(2 * 1.2)
-
-        if(distanceToZero > self.authority):
-            if(self.serviceBrake == False):
-                self.servBrakeToggle(self)
-            
-            while(self.currentSpeed != 0):
-                self.currentSpeed = self.currentSpeed - 1.2
-                time.sleep(1)
