b309e18412dead20e3dc1ed3393aabc06cfc6050
Prashanth-Kumaran <131703133+Prashanth-Kumaran@users.noreply.github.com>
2024-10-29 14:46:06 -0400

    changed green yard PLC to hard code
    hard coded values to reduce bugs

--- Wayside_Controller/GreenYardPLC.py
+++ Wayside_Controller/GreenYardPLC.py
@@ -1,105 +1,106 @@
-
 class GreenYardPLC:
     def __init__(self):
-        self.switch_58 = False
-        self.switch_62 = False
-        self.authority = [False for i in range(27)]
-        self.occupancy = [False for i in range(33)]
+        self.occupancy = [False for i in range(36)]
+        self.authority = [True for i in range(28)]
+        self.switch_57 = False
+        self.switch_63 = False
+        self.maintenance = [False for i in range(36)]
 
-        # 2 dimensional lists to represent occupancy and authorities split into zones
-        self.zone_occ = [self.occupancy[:6], self.occupancy[6:16], self.occupancy[16:22], self.occupancy[22:27], self.occupancy[27:]]
-        self.zone_auth = [self.authority[:6], self.authority[6:16], self.authority[16:22], self.authority[22:]]
-        self.maint_zones = [self.occupancy[:6], self.occupancy[6:16], self.occupancy[16:22], self.occupancy[22:27], self.occupancy[27:]]
-   
-    # takes inputted occupancy list and splits it into zones
     def update_occupancy(self, new_occ):
         self.occupancy = new_occ
-        self.zone_occ = [self.occupancy[:6], self.occupancy[6:16], self.occupancy[16:22], self.occupancy[22:27], self.occupancy[27:]]
-        self.update_authority()
-        return self.authority
-
-    # takes suggested maintenance list and checks for safety, then updates maintenance 2d list
-    def maintenance(self, maint_sugg):
-        maint_sugg_zones = [maint_sugg[:6], maint_sugg[6:16], maint_sugg[16:22], maint_sugg[22:27], maint_sugg[27:]]
-        for i in range(5):
-            for j in range(len(self.maint_zones[i])):
-                if maint_sugg_zones[i][j] == True and self.maint_zones[i][j] == False:
-                    if any(self.zone_occ[i][k] == True and self.maint_zones[i][k] == False for k in range(len(self.maint_zones[i]))):
-                        self.maint_zones[i][j] = False
-                        return False
-                    else:
-                        self.maint_zones[i][j] = True
-                        self.zone_occ[i][j] = True
-                        return True
-                if maint_sugg_zones[i][j] == False and self.maint_zones[i][j] == True:
-                    self.maint_zones[i][j] = False
-                    self.zone_occ[i][j] = False
-                    return True
-        # reconstructs occupancy list based on zone occupancy 2d list
-        self.occupancy = []
-        for zone in self.zone_occ:
-            for block in zone:
-                self.occupancy.append(block)
-
-        # updates authorities based on new maintenance blocks
-        self.update_authority()
-        return self.authority
     
-    def switch_suggestion(self, switch):
-        if switch == True:
-            if self.switch_58 and self.switch_62:
-                self.update_authority()
-                return True
-            self.switch_58 = True
-            self.switch_62 = True
-            self.update_authority()
+    def update_authority(self):
+        for i in range(6):
+            if any(self.occupancy[6:17]):
+                self.authority[i] = False
+            else:
+                self.authority[i] = True
+
+        for i in range(6, 17):
+            if any(self.occupancy[17:22]) and self.switch_57 == True:
+                self.authority[i] = False
+            else:
+                self.authority[i] = True     
+
+        for i in range(17, 22):
+            if any(self.occupancy[22:28]) or self.switch_63 == False:
+                self.authority[i] = False
+            else:
+                self.authority[i] = True    
+
+        for i in range(22, 28):
+            if any(self.occupancy[28:]):
+                self.authority[i] = False
+            else:
+                self.authority[i] = True    
+
+    def update_switches(self, suggested_switch):
+        if suggested_switch == True:
+            self.switch_57 = True
+            self.switch_63 = True
             return True
-        
-        if switch == False:
-            if any(self.zone_occ[3]):
-                self.switch_58 = True
-                self.switch_62 = True
-                self.update_authority()
+        elif suggested_switch == False:
+            if any(self.occupancy[17:22]):
+                self.switch_57 = True
+                self.switch_63 = True
                 return False
             else:
-                self.switch_58 = False
-                self.switch_62 = False
-                self.update_authority()
+                self.switch_57 = False
+                self.switch_63 = False
                 return True
-        
-            
-    def update_authority(self):
-        next_zone = iter(self.zone_occ)
-        next(next_zone)
-
-        #initially sets the authority all occupancies to true
-        self.authority = self.occupancy[:27]
-        self.zone_auth = [self.authority[:6], self.authority[6:16], self.authority[16:22], self.authority[22:]]
-
-        # sets the all the auth of the current zone to false if the next zone has any occupancy
-        for i in range(4):
-            if any(next(next_zone)):
-                self.zone_auth[i] = [False for j in range(len(self.zone_auth[i]))]
-        
-        # sets the authority of zone 3 to false if the switch is not connected to the line
-        # This is a redunant check as the switch should always be set to true if there is an occupancy in zone 3
-        if self.switch_62 == False:
-            self.zone_auth[3] = [False for i in range(5)]
-
-        # problem for later: set auth of zone to false if occupancy is detected in the same zone
-
-        # updates authority based on zone_auth
-        self.authority = []
-        for zone in self.zone_auth:
-            for block in zone:
-                self.authority.append(block)
     
-
+    def update_maintenance(self, new_maint):
+        for i in range(6):
+            if new_maint[i] == True and self.maintenance[i] == False:
+                maint_safety = True
+                for j in range(17):
+                    if self.occupancy[j] == True and self.maintenance[j] == False:
+                        maint_safety = False
+                        break
+                if maint_safety == True:
+                    self.maintenance[i] = True
+                    self.occupancy[i] = True
+            elif new_maint[i] == False and self.maintenance[i] == True:
+                self.maintenance[i] = False
+                self.occupancy[i] = False
+            
+        for i in range(6, 17):
+            if new_maint[i] == True and self.maintenance[i] == False:
+                maint_safety = True
+                for j in range(6, 22):
+                    if self.occupancy[j] == True and self.maintenance[j] == False:
+                        maint_safety = False
+                        break
+                if maint_safety == True:
+                    self.maintenance[i] = True
+                    self.occupancy[i] = True
+            elif new_maint[i] == False and self.maintenance[i] == True:
+                self.maintenance[i] = False
+                self.occupancy[i] = False
+        
+        for i in range(17, 22):
+            if new_maint[i] == True and self.maintenance[i] == False:
+                maint_safety = True
+                for j in range(17, 28):
+                    if self.occupancy[j] == True and self.maintenance[j] == False:
+                        maint_safety = False
+                        break
+                if maint_safety == True:
+                    self.maintenance[i] = True
+                    self.occupancy[i] = True
+            elif new_maint[i] == False and self.maintenance[i] == True:
+                self.maintenance[i] = False
+                self.occupancy[i] = False
+        
+        for i in range(22, 28):
+            if new_maint[i] == True and self.maintenance[i] == False:
+                maint_safety = True
+                for j in range(22, 36):
+                    if self.occupancy[j] == True and self.maintenance[j] == False:
+                        maint_safety = False
+                        break
+                if maint_safety == True:
+                    self.maintenance[i] = True
+                    self.occupancy[i] = True
 
             
-    def say_hi(self):
-        print("PLC Uploaded Successfully")
-
-
-if __name__ == "__main__":
-    plc = GreenYardPLC()
