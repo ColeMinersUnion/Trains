cbfcbc434ef79ea27460fb63c938dd3c39b0aa44
hnes1885 <hnes1885@gmail.com>
2024-11-22 15:40:07 -0500

    software wayside UI implementation in integration file and shell

--- tk_ws_integration.py
+++ tk_ws_integration.py
@@ -11,7 +11,7 @@ def main():
     tm_signals = SignalHandler()
 
     # Show both windows
-    ws_window.ui.show()
+    ws_window.show()
     tm_window.show()
 
     ws_window.wss_tm_switch_13.connect(tm_signals.getSwitch13)
