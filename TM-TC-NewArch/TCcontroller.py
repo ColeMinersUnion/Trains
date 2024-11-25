4b0ac0d6494a6522b581e21e64d880e843c3eea2
hannahgrace117 <126520259+hannahgrace117@users.noreply.github.com>
2024-11-25 11:11:23 -0500

    Update TCcontroller.py

--- TM-TC-NewArch/TCcontroller.py
+++ TM-TC-NewArch/TCcontroller.py
@@ -1,5 +1,5 @@
 # train_controller/controller.py
-from PyQt6.QtCore import QObject
+from PyQt5.QtCore import QObject
 
 class TCcontroller (QObject):
     def __init__(self, model, view):
@@ -8,5 +8,6 @@ class TCcontroller (QObject):
         self.view = view
 
         # Connect the power command signal from the view to the model's power command
-        self.view.setpoint_command_signal.connect(self.model.set_setpoint)  
+        self.view.setpoint_command_signal.connect(self.model.set_commanded_speed)  
         self.view.pid_tick_signal.connect(self.model.pid_tick)
+        self.view.ebrake_signal.connect(self.model.set_ebrake)
