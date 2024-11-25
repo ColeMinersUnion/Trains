280a47e0b108629a05172e77d716a046d4125a37
hnes1885 <hnes1885@gmail.com>
2024-11-25 12:14:22 -0500

    element state table updates again

--- Wayside_Controller/Software/SoftwareShell.py
+++ Wayside_Controller/Software/SoftwareShell.py
@@ -38,8 +38,8 @@ class WaysideShell(QMainWindow):
 
         self.plc = GreenPLC()
         
-        self.occupancy = [False for i in range(1,151)]
-        self.authority = [False for i in range(1,151)]
+        self.occupancy = [False for i in range(151)]
+        self.authority = [False for i in range(151)]
         #switches
         self.switch_13 = True
         self.switch_28 = False
@@ -61,7 +61,7 @@ class WaysideShell(QMainWindow):
 
         '''self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update_ui) 
-        self.timer.start(15)''' 
+        self.timer.start(15)'''
 
     
     #slot to update occupancy which then updates the plc authority
@@ -102,42 +102,52 @@ class WaysideShell(QMainWindow):
     def toggle_switch_13(self):
         self.switch_13 = not self.switch_13
         self.wss_tm_switch_13.emit(self.switch_13)
+        self.update_ui()
     
     def toggle_switch_28(self):
         self.switch_28 = not self.switch_28
         self.wss_tm_switch_28.emit(self.switch_28)
+        self.update_ui()
 
     def toggle_switch_77(self):
         self.switch_77 = not self.switch_77
         self.wss_tm_switch_77.emit(self.switch_77)
+        self.update_ui()
     
     def toggle_switch_85(self):
         self.switch_85 = not self.switch_85
         self.wss_tm_switch_85.emit(self.switch_85)
+        self.update_ui()
 
     def toggle_signal_13(self):
         self.signal_13 = not self.signal_13
         self.wss_tm_signal_13.emit(self.signal_13)
+        self.update_ui()
     
     def toggle_signal_28(self):
         self.signal_28 = not self.signal_28
         self.wss_tm_signal_28.emit(self.signal_28)
+        self.update_ui()
     
     def toggle_signal_77(self):
         self.signal_77 = not self.signal_77
         self.wss_tm_signal_77.emit(self.signal_77)
+        self.update_ui()
 
     def toggle_signal_85(self):
         self.signal_85 = not self.signal_85
         self.wss_tm_signal_85.emit(self.signal_85)
+        self.update_ui()
     
     def toggle_crossing_19(self):
         self.crossing_19 = not self.crossing_19
         self.wss_tm_crossing_19.emit(self.crossing_19)
+        self.update_ui()
     
     def toggle_crossing_108(self):
         self.crossing_108 =  not self.crossing_108
         self.wss_tm_crossing_108.emit(self.crossing_108)
+        self.update_ui()
 
     def manual_inputs(self):
         self.manual_sw13_button.clicked.connect(self.toggle_switch_13)
@@ -155,10 +165,10 @@ class WaysideShell(QMainWindow):
     
     def update_ui(self):
         #update block table state and authority from Track Model
-        for i in range(len(self.occupancy)):
+        for i in range(0,150):
             self.wayside_block_table.setItem(i,0, QTableWidgetItem(str(self.occupancy[i])))
 
-        for i in range(len(self.authority)):
+        for i in range(0,150):
             self.wayside_block_table.setItem(i,1, QTableWidgetItem(str(self.authority[i])))
         
         #update switches 
