88fcbf83ed0ee4e2a6cfd557d34613a76a5c86a0
hannahgrace117 <126520259+hannahgrace117@users.noreply.github.com>
2024-11-26 12:07:17 -0500

    add authority

--- TM_TC_NewArch/TCcontroller.py
+++ TM_TC_NewArch/TCcontroller.py
@@ -2,15 +2,18 @@
 from PyQt6.QtCore import QObject
 
 class TCcontroller (QObject):
-    def __init__(self, model, view):
+    def __init__(self, model, view, authority):
         super().__init__()
         self.model = model
         self.view = view
+        self.model.set_full_authority(authority)
 
         # Connect the power command signal from the view to the model's power command
         self.view.setpoint_command_signal.connect(self.model.set_commanded_speed)  
         self.view.pid_tick_signal.connect(self.model.pid_tick)
         self.view.ebrake_signal.connect(self.model.set_ebrake)
-
         self.view.kp_signal.connect(self.model.set_Kp)
         self.view.ki_signal.connect(self.model.set_Ki)
+        
+        self.model.power_command.connect(self.view.pwr_updated)
+        self.model.update_auth.connect(self.view.full_auth)
