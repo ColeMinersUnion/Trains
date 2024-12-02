464b26922e3272d622af7d523845763539f95315
ColeMinersUnion <colehansen018@gmail.com>
2024-12-02 17:14:07 -0500

    Stuff

--- CTC_WS.py
+++ CTC_WS.py
@@ -3,6 +3,8 @@ from CTC_Office.Frontend.app import CTCApplication
 from PyQt6.QtWidgets import QApplication
 from Wayside_Controller.Software.SoftwareShell import WaysideShell as SoftwareShell
 from Wayside_Controller.Hardware.HardwareShell import WaysideWindow as HardwareShell
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
@@ -12,6 +14,7 @@ if __name__ == "__main__":
     
     #Emit Connect Slot
     ex2.wss_ctc_occupancy.connect(ex.updateOccupancy)
+    ex3.ws_ctc_occupancy.connect(ex.updateOccupancy)
     ex3.update_switch.connect(ex.handleGreenOutputSwitch)
 
     ex.show()
