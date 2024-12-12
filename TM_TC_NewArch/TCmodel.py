39d00aa9be1757bae0f2b904962072e0902fefa2
hannahgrace117 <126520259+hannahgrace117@users.noreply.github.com>
2024-12-12 00:14:31 -0500

    slay

--- TM_TC_NewArch/TCmodel.py
+++ TM_TC_NewArch/TCmodel.py
@@ -227,7 +227,7 @@ class TCmodel(QObject):
         go_nogo = input[self.blocks]
         self.gonogo_display.emit(go_nogo)
         #check if wayside stop is enabled
-        if (go_nogo == 1):
+        if (go_nogo == True):
             self.cut_power_and_enable_brake()
             self.stopped_by_wayside = 1
         else:
