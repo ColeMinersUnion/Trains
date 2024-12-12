a66e6e410f301b2a86e31015b345c1958486bbe1
hannahgrace117 <126520259+hannahgrace117@users.noreply.github.com>
2024-12-11 23:30:38 -0500

    switched to pyqt6

--- TM_TC_NewArch/TCmodel.py
+++ TM_TC_NewArch/TCmodel.py
@@ -1,5 +1,5 @@
 # train_controller/model.py
-from PyQt5.QtCore import QObject, pyqtSignal as Signal, pyqtSlot as Slot, QTimer
+from PyQt6.QtCore import QObject, pyqtSignal as Signal, pyqtSlot as Slot, QTimer
 import os, time as timer
 
 T = 0.125  # Period of control loop in seconds 
